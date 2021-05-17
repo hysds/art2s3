fa68733fca5ab1fd7f832b6af2ef9ea99abad189
Gerald Manipon <pymonger@gmail.com>
2021-05-17 09:59:26 -0700

    set cli script

--- art2s3/sync.py
+++ art2s3/sync.py
@@ -3,7 +3,7 @@ from artifactory import ArtifactoryPath
 
 
 @click.command()
-@click.option('--path', help="Artifactory path")
+@click.argument('path')
 @click.option('--api_key', help="Artifactory API key")
 def walk(path, api_key):
     path = ArtifactoryPath(path, apikey=api_key)
